1d6e9c4009a4af50b8d4cc081eaa9e16cba5afd2
jorgemindtec <jorgearellano@mindtec.com.mx>
2017-08-01 13:42:05 -0600

    Update functions.js

--- www/js/functions.js
+++ www/js/functions.js
@@ -52,6 +52,7 @@ function menu(dato){
 		document.getElementById("tittle01").className="hide";
 		document.getElementById("tittle02").className="display";
 		document.getElementById("tittle03").className="hide";
+		MapaGps();
 	}
 	else if(dato==3){
 		document.getElementById("menu03").style.backgroundColor="#f6892e";
@@ -285,3 +286,5 @@ function sender(dato){
 	document.getElementById("form7").className="display";		
 	}	
 }
+
+
